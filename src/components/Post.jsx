50b1ee6c2aeb7f253a037360015979f063501a41
William Bebiano <william.bebiano@hotmail.com>
2023-05-24 16:38:09 +0200

    Finalizando estrutura staticas dos components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post(){
     <article className={styles.post}> 
     <header>
         <div className={styles.author} >
-          <img src="https://github.com/WilliamBebiano.png" alt='' />
+          <Avatar src="https://github.com/WilliamBebiano.png" alt='' />
           <div className={styles.authorInfo}>
             <strong>William Bebiano</strong>
             <span>Web Developer</span>
